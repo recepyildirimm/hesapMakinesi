9432d6b0954b6e0686bfc34128eae1d77d4c084b
Recep YILDIRIM <ryildirimm45@gmail.com>
2022-04-04 22:04:51 +0300

    bolme fonksiyonu eklendi

--- toplama.cpp
+++ toplama.cpp
@@ -1,18 +1,41 @@
 #include <stdio.h>
 int toplama(int a,int b);
+int bolme(int a,int b);
 int main(){
 int sayi1,sayi2;
+char islem;
 
 printf("HESAP MAKINESI\n");
-//printf("Yapacaðýnýz iþlemi secin\n");
-//printf("Toplama iþlemi için 1 giriniz ");
-printf("Toplama islemi icin 2 sayi giriniz \n");
+printf("Yapacaginiz islemi seciniz\n");
+printf("TOPLAMA ISLEMI '+' \nBOLME ISLEMI '/' ");
+scanf("%c",&islem);
+switch(islem)
+{
+	case '+':
+		printf("Toplama islemi icin 2 sayi giriniz \n");
+		scanf("%d",&sayi1);
+		scanf("%d",&sayi2);
+		toplama(sayi1,sayi2);
+		printf("Sonuc=%d",toplama(sayi1,sayi2));
+		break;
+	case '/':
+		printf("Bolme islemi icin 2 sayi giriniz \n");
+		scanf("%d",&sayi1);
+		scanf("%d",&sayi2);
+		toplama(sayi1,sayi2);
+		printf("Sonuc=%d",bolme(sayi1,sayi2));
+		break;
+}
 
-scanf("%d",&sayi1);
-scanf("%d",&sayi2);
-printf("Sonuc=%d",toplama(sayi1,sayi2));
+
+
+
 }
 
 int toplama(int a,int b){
 	return a+b;
+}
+
+int bolme(int a,int b){
+	return a/b;	
 }
